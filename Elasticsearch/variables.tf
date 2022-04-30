dabfc6042213ce3a992e2bb41f6d4e3dbc5d82fe
pramodtoom <pramodtoom@gmail.com>
2022-04-30 15:30:20 +0000

    search

---
+++ Elasticsearch/variables.tf
@@ -0,0 +1,33 @@
+# variable "create_iam_service_linked_role" {}
+# variable "enabled" {}
+# # variable "RoleName" {}
+# variable "vpc_id" {}
+# variable "cidr" {}
+# variable "aws_subnet" {}
+# variable "domain_name" {}
+# variable "cluster-name" {}
+# variable "es_version" {}
+# variable "elastic_instance_type" {}
+# variable "instance_count" {}
+# variable "dedicated_master_type" {}
+# variable "es_zone_awareness" {}
+# variable "es_zone_awareness_count" {}
+# variable "dedicated_master_threshold" {}
+# variable "node_to_node_encryption_enabled" {}
+# variable "ebs_volume_size" {}
+# variable "ebs_volume_type" {}
+# variable "snapshot_start_hour" {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
