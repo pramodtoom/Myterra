5d033d55c7c53c3af73d5caa92261964b04ec1a4
EC2 Default User <ec2-user@ip-172-31-21-0.ec2.internal>
2022-06-01 12:55:48 +0000

    EKS

---
+++ EKS/variable.tf
@@ -0,0 +1,14 @@
+#----EKS variables.tf----
+
+variable "cluster-name" {}
+
+variable "eks-cluster-role-name" {}
+
+variable "eks-securitygroup" {}
+
+variable "vpc_id" {}
+
+variable "aws_subnets" {}
+
+
+
